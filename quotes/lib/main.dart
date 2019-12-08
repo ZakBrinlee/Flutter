1143e5293891cd8ed098a8fa4533722d86991b62
ZakBrinlee <zbrinlee@hotmail.com>
2019-12-07 23:31:15 -0800

    Quote app with functions as parameters

---
+++ quotes/lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'quote.dart';
+import 'quote_card.dart';
+
+void main() => runApp(MaterialApp(
+  home: QuoteList(),
+));
+
+class QuoteList extends StatefulWidget {
+  @override
+  _QuoteListState createState() => _QuoteListState();
+}
+
+class _QuoteListState extends State<QuoteList> {
+
+  List<Quote> quotes = [
+    Quote(author: 'Author 1', text: 'Text 1'),
+    Quote(author: 'Author 2', text: 'Text 2'),
+    Quote(author: 'Author 3', text: 'Text 3')
+
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        title: Text('Awesome Quotes'),
+        centerTitle: true,
+        backgroundColor: Colors.redAccent,
+      ),
+      body: Column(
+        children: quotes.map((quote) => QuoteCard(
+            quote: quote,
+            delete: () {
+              setState(() {
+                quotes.remove(quote);
+              });
+            }
+        )).toList(),
+      ),
+    );
+  }
+}
